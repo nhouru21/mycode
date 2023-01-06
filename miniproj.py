668220c555f1686e4f74ec2540d48709a223ef0a
nhouru21 <njhoughton5@gmail.com>
2023-01-06 15:22:57 +0000

    update miniproj

--- miniproj.py
+++ miniproj.py
@@ -6,7 +6,19 @@ def main():
 
     #user gets to pick the range of numbers to guess from
     print("Welcome to the number guessing game where you get to pick the range of numbers you can guess from!")
-    num_range = int(input("Please enter the ending number of your number range (e.g. if you pick 1000, the range is from 1-1000)"))
+
+    #validation that user enters an int and is at least 2 or greater
+    while True:
+        try:
+            num_range = int(input("Please enter the ending number of your number range (e.g. if you pick 1000, the range is from 1-1000)")) 
+            if num_range > 1:
+               break
+            else:
+                print("Invalid number, please try again")
+        except:
+            print("Please enter an integer, try again")
+            
+
     num = random.randrange(1,num_range)
     guess = int(input("Please enter a number inclusive of your min and max range values: "))
     #simple counter for number of guesses
@@ -19,13 +31,29 @@ def main():
         if guess>num:
             #if the guess is to high
             print("You are too high!")
-            guess = int(input("Please guess again: "))
+            guess = int(input(f"You have made {i} guesses, please guess again: "))
         elif guess<num:
             #if the guess is to low
             print("You are too low!")
-            guess = int(input("Please guess again: "))
+            guess = int(input(f"You have made {i} guesses, please guess again: "))
         else:
             break
     print("Your guess is correct!")
 
+'''
+I was wanting to add more vaildation here for the guesses but ran out of time
+def guess_val(i,guess):
+    while True:
+        try:
+            guess = int(input(f"You have made {i} guesses, please guess again"))
+            if guess >= 1:
+                break
+            else:
+                print("Invalid number, please try again")
+        except:
+            print("Please enter an integer, try again")
+'''
+
 main()
+
+

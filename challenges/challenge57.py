a801d78573a2c010fe831000f03c8ff1d5a87630
nhouru21 <njhoughton5@gmail.com>
2023-01-06 21:38:36 +0000

    studying for logic

---
+++ challenges/challenge57.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import html
+
+def main():
+    trivia= {
+         "category": "Entertainment: Film",
+         "type": "multiple",
+         "question": "Which of the following is NOT a quote from the 1942 film Casablanca? ",
+         "correct_answer": "&quot;Frankly, my dear, I don&#039;t give a damn.&quot;",
+         "incorrect_answers": [
+             "&quot;Here&#039;s lookin&#039; at you, kid.&quot;",
+             "&ldquo;Of all the gin joints, in all the towns, in all the world, she walks into mine&hellip;&rdquo;",
+             "&quot;Round up the usual suspects.&quot;"
+            ]
+        }
+question = html.unescape(trivia["question"])
+print(question)
+main()

ea160dbead90f1896822ed4a4e8aa6dd135e2fdd
nhouru21 <117097365+nhouru21@users.noreply.github.com>
2023-01-08 07:01:54 -0800

    Delete iftest directory

--- iftest/condition01.py
+++
@@ -1,9 +0,0 @@
-    #!/usr/bin/env python3
-
-    # create the string hostname
-hostname = "MTG"
-    # test logic with the `if` statement
-    # what to do if this statement is found to be true
-if hostname == "MTG":
-    print("The hostname was found to be MTG")
-

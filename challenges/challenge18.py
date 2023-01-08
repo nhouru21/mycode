a696998ec662effa24ae57a23934a2d19043206c
nhouru21 <njhoughton5@gmail.com>
2023-01-08 19:33:35 +0000

    input / print challenge

---
+++ challenges/challenge18.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    user_name = input("What is your name?: ")
+    day = input("What day of the week is it?: ")
+
+    print(f"Hello {user_name}! Happy {day}")
+
+main()

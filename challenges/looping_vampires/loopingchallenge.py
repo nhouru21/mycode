9a837e02056a13add3babf8ee5c2a2b35bae91cb
nhouru21 <njhoughton5@gmail.com>
2023-01-08 16:54:51 +0000

    add looping vampires challenge

---
+++ challenges/looping_vampires/loopingchallenge.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" looping vampires challenge """
+
+def main():
+    counter= 0
+
+    with open("dracula.txt","r") as foo:
+        with open("vampytimex.txt","w") as fang:
+            for line in foo:
+                if "vampire" in line.lower():
+                    print(line)
+                    counter += 1
+                    fang.write(line)
+
+    print(counter)
+
+
+main()
